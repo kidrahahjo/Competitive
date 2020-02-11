483dce4d10ad5fd17bfa8df4bfb53f244333b5d7
kidrahahjo <kidrahahjo@gmail.com>
2020-02-11 19:16:31 +0530

    Adding Environment

---
+++ Env/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;  
+
+
+int check(string n,int l){
+  int count = 0;
+  for(int i = 0;i<l;i++){
+    if(n[i]=='B'){
+      count+=1;
+    }else{
+      break;
+    }
+  }
+  return count;
+}
+
+int main()  
+{
+  #ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+	freopen("err.txt","w",stderr);
+  #endif
+  cout<<"Hello World!";
+}
